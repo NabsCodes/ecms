057f5f2ca3927f3155ca2ae436c69927fd75c23e
Nabeel Hassan <hassanhauda@gmail.com>
2023-11-25 17:04:36 +0100

    first commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,43 +6,43 @@ const jwt = require('jsonwebtoken');
 const Users = require('../models/User');
 
 function auth(req, res, next) {
-    
+
     try {
-         const token = req.headers.authorization;
+        const token = req.headers.authorization;
         //check for token
-        if (!token) {res.json({status:false, msg: "No token, authorization denied" })}
-        else{
-        //verify token
+        if (!token) { res.json({ status: false, msg: "No token, authorization denied" }); }
+        else {
+            //verify token
             const decoded = jwt.verify(token, process.env.jwtSecret);
             //add user from payload
             req.user = decoded;
-            next();  
+            next();
         };
-        
+
     } catch (e) {
-        res.json({status:false, msg: 'token is not valid' });
+        res.json({ status: false, msg: 'token is not valid' });
     }
 
 }
 async function authDetail(req, res, next) {
 
     try {
-         const token = req.headers.authorization;
+        const token = req.headers.authorization;
         //check for token
-        if (!token) {res.status(401).send({ status:false, msg: "No token, authorization denied" })}
-        else{
-        //verify token
+        if (!token) { res.status(401).send({ status: false, msg: "No token, authorization denied" }); }
+        else {
+            //verify token
             const decoded = await jwt.verify(token, process.env.jwtSecret);
             //add user from payload
-            if(decoded.id){
+            if (decoded.id) {
                 const user = await Users.findById(decoded.id);
                 req.user = user;
-                next();  
-            }else{
-                res.status(401).send({ status:false, msg: "Error Verifying Token, authorization denied" });
+                next();
+            } else {
+                res.status(401).send({ status: false, msg: "Error Verifying Token, authorization denied" });
             }
         };
-        
+
     } catch (e) {
         console.log(e);
         res.status(400).send({ msg: 'token is not valid' });
@@ -50,4 +50,4 @@ async function authDetail(req, res, next) {
 
 }
 
-module.exports = {auth,authDetail};
+module.exports = { auth, authDetail };
